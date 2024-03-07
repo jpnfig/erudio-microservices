e07fc30f719053711d01a28f8639963c538e20ac
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2024-03-07 14:38:38 -0300

    Adding Swagger OpenAPI with microservices

--- 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -7,15 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("book-service")
+@Tag(name = "Foo bar")
 public class FooBarController {
 	
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
 	@GetMapping("/foo-bar")
+	@Operation(summary = "Foo bar")
 	//@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 	//@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
 	//@RateLimiter(name = "default")
